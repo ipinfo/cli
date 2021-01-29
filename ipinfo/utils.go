bf088bec59aabb57c35746c827a2d1f332330533
Uman Shahzad <uman@mslm.io>
2021-01-29 12:17:18 +0500

    add a cidr-to-ips func.
    we will use this in range inputs and a prips cmd.

--- ipinfo/utils.go
+++ ipinfo/utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -117,4 +118,24 @@ func outputFriendlyCore(d *ipinfo.Core) {
 			}
 		}
 	}
+}
+
+func cidrToIPs(cidrStr string) ([]net.IP, error) {
+	_, ipnet, err := net.ParseCIDR(cidrStr)
+	if err != nil {
+		return nil, err
+	}
+
+	mask := binary.BigEndian.Uint32(ipnet.Mask)
+	start := binary.BigEndian.Uint32(ipnet.IP)
+	finish := (start & mask) | (mask ^ 0xffffffff)
+
+	ips := make([]net.IP, 0, finish-start+1)
+	for i := start; i <= finish; i++ {
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, i)
+		ips = append(ips, ip)
+	}
+
+	return ips, nil
 }
