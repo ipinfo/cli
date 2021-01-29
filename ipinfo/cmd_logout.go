aadbe99985700375c43c1eb8901d51f0b0ced36e
Uman Shahzad <uman@mslm.io>
2021-01-29 21:41:38 +0500

    feedback for when we log out

--- ipinfo/cmd_logout.go
+++ ipinfo/cmd_logout.go
@@ -10,7 +10,10 @@ func cmdLogout(c *cli.Context) error {
 	// delete but don't return an error; just log it.
 	if err := deleteToken(); err != nil {
 		fmt.Println("not logged in")
+		return nil
 	}
+
+	fmt.Println("logged out")
 
 	return nil
 }
