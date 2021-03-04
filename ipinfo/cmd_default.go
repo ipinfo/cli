9043b3128468f498a60422d36a92364885f18241
Uman Shahzad <uman@mslm.io>
2021-03-04 07:56:28 +0500

    Use bulk lookup for stdin when no command.

---
+++ ipinfo/cmd_default.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/ipinfo/go/v2/ipinfo"
+	"github.com/spf13/pflag"
+)
+
+func printHelpDefault() {
+	fmt.Printf(
+		`Usage: %s <cmd> [<opts>] [<args>]
+
+Commands:
+  <ip>        look up details for an IP address, e.g. 8.8.8.8.
+  <asn>       look up details for an ASN, e.g. AS123 or as123.
+  myip        get details for your IP.
+  bulk        get details for multiple IPs in bulk.
+  sum         get summarized data for a group of IPs.
+  prips       print IP list from CIDR or range.
+  login       save an API token session.
+  logout      delete your current API token session.
+  version     show current version.
+
+Options:
+  General:
+    --token <tok>, -t <tok>
+      use <tok> as API token.
+    --help, -h
+      show help.
+
+  Outputs:
+    --pretty, -p
+      output pretty format.
+    --json, -j
+      output JSON format.
+    --csv, -c
+      output CSV format.
+`, progBase)
+}
+
+func cmdDefault() (err error) {
+	var ips []net.IP
+	var fTok string
+	var fHelp bool
+	var fPretty bool
+	var fJSON bool
+	var fCSV bool
+
+	pflag.StringVarP(&fTok, "token", "t", "", "the token to use.")
+	pflag.BoolVarP(&fHelp, "help", "h", false, "show help.")
+	pflag.BoolVarP(&fPretty, "pretty", "p", true, "output pretty format.")
+	pflag.BoolVarP(&fJSON, "json", "j", true, "output JSON format. (default)")
+	pflag.BoolVarP(&fCSV, "csv", "c", false, "output CSV format.")
+	pflag.Parse()
+
+	if fHelp {
+		printHelpDefault()
+		return nil
+	}
+
+	args := pflag.Args()
+	if len(args) != 0 && args[0] != "-" {
+		fmt.Printf("err: \"%s\" is not a command.\n", os.Args[1])
+		fmt.Println()
+		printHelpDefault()
+		return nil
+	}
+
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) != 0 {
+		printHelpDefault()
+		return nil
+	}
+
+	if err := prepareIpinfoClient(fTok); err != nil {
+		return err
+	}
+
+	ips = ipsFromStdin()
+
+	if len(ips) == 0 {
+		fmt.Println("no input ips")
+		return nil
+	}
+
+	data, err := ii.GetIPInfoBatch(ips, ipinfo.BatchReqOpts{})
+	if err != nil {
+		return err
+	}
+
+	if fCSV {
+		return outputCSVBatchCore(data)
+	}
+
+	return outputJSON(data)
+}
--- ipinfo/main.go
+++ ipinfo/main.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/ipinfo/go/v2/ipinfo"
-	"github.com/spf13/pflag"
 )
 
 var progBase = filepath.Base(os.Args[0])
@@ -23,46 +22,14 @@ func prepareIpinfoClient(tok string) error {
 	return nil
 }
 
-func printHelp() {
-	fmt.Printf(
-		`Usage: %s <cmd> [<opts>] [<args>]
-
-Commands:
-  <ip>        look up details for an IP address, e.g. 8.8.8.8.
-  <asn>       look up details for an ASN, e.g. AS123 or as123.
-  myip        get details for your IP.
-  bulk        get details for multiple IPs in bulk.
-  sum         get summarized data for a group of IPs.
-  prips       print IP list from CIDR or range.
-  login       save an API token session.
-  logout      delete your current API token session.
-  version     show current version.
-
-Options:
-  General:
-    --token <tok>, -t <tok>
-      use <tok> as API token.
-    --help, -h
-      show help.
-
-  Outputs:
-    --pretty, -p
-      output pretty format.
-    --json, -j
-      output JSON format.
-    --csv, -c
-      output CSV format.
-`, progBase)
-}
-
 func main() {
-	if len(os.Args) == 1 {
-		printHelp()
-		return
+	var err error
+	var cmd string
+
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
 	}
 
-	var err error
-	cmd := os.Args[1]
 	switch {
 	case isIP(cmd):
 		err = cmdIP(cmd)
@@ -84,18 +51,7 @@ func main() {
 	case cmd == "version":
 		err = cmdVersion()
 	default:
-		var fHelp bool
-
-		pflag.BoolVarP(&fHelp, "help", "h", false, "show help.")
-		pflag.Parse()
-
-		if fHelp {
-			printHelp()
-		} else {
-			fmt.Printf("err: \"%s\" is not a command.\n", cmd)
-			fmt.Println()
-			printHelp()
-		}
+		err = cmdDefault()
 	}
 
 	if err != nil {
