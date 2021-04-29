2624c1581c797d3fb3795f4375acc0b1ec3bb91f
Uman Shahzad <uman@mslm.io>
2021-04-29 17:21:14 +0500

    some output improvements to macos script

--- ipinfo/macos.sh
+++ ipinfo/macos.sh
@@ -4,7 +4,11 @@ VSN=1.1.0
 PLAT=darwin_amd64
 
 curl -LO https://github.com/ipinfo/cli/releases/download/ipinfo-${VSN}/ipinfo_${VSN}_${PLAT}.tar.gz
-tar -xvf ipinfo_${VSN}_${PLAT}.tar.gz
+tar -xf ipinfo_${VSN}_${PLAT}.tar.gz
 rm ipinfo_${VSN}_${PLAT}.tar.gz
 mv ipinfo_${VSN}_${PLAT} /usr/local/bin/ipinfo
+
+echo
+echo 'You can now run `ipinfo`'.
+
 rm $0
