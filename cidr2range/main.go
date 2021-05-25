1ef1a52b8a91905617b875fa45ed2a46edab9724
Uman Shahzad <uman@mslm.io>
2021-05-26 04:14:59 +0500

    describe api for cidr2range.

--- cidr2range/main.go
+++ cidr2range/main.go
@@ -15,7 +15,32 @@ var version = "1.0.0"
 
 func printHelp() {
 	fmt.Printf(
-		`Usage: %s [<opts>]
+		`Usage: %s [<opts>] <cidr | filepath>
+
+Description:
+
+  Accepts CIDRs and file paths to files containing CIDRs, converting them all
+  to IP ranges.
+
+  If a file is input, it is assumed that the CIDR to convert is the first entry
+  of each line (separated by '\n'). All other data remains the same.
+
+  The IP range output is of the form "<start>-<end>" where "<start>" comes
+  before or is equal to "<end>" in numeric value.
+
+Examples:
+
+  # Get the range for CIDR 1.1.1.0/30.
+  $ %[1]s 1.1.1.0/30
+
+  # Convert CIDR entries to IP ranges in 2 files.
+  $ %[1]s /path/to/file1.txt /path/to/file2.txt
+
+  # Convert CIDR entries to IP ranges from stdin.
+  $ cat /path/to/file1.txt | %[1]s
+
+  # Convert CIDR entries to IP ranges from stdin and a file.
+  $ cat /path/to/file1.txt | %[1]s /path/to/file2.txt
 
 Options:
   --version, -v
--- ipinfo/cmd_cidr2range.go
+++ ipinfo/cmd_cidr2range.go
@@ -9,7 +9,32 @@ import (
 
 func printHelpCIDR2Range() {
 	fmt.Printf(
-		`Usage: %s cidr2range [<opts>]
+		`Usage: %s cidr2range [<opts>] <cidr | filepath>
+
+Description:
+
+  Accepts CIDRs and file paths to files containing CIDRs, converting them all
+  to IP ranges.
+
+  If a file is input, it is assumed that the CIDR to convert is the first entry
+  of each line (separated by '\n'). All other data remains the same.
+
+  The IP range output is of the form "<start>-<end>" where "<start>" comes
+  before or is equal to "<end>" in numeric value.
+
+Examples:
+
+  # Get the range for CIDR 1.1.1.0/30.
+  $ %[1]s cidr2range 1.1.1.0/30
+
+  # Convert CIDR entries to IP ranges in 2 files.
+  $ %[1]s cidr2range /path/to/file1.txt /path/to/file2.txt
+
+  # Convert CIDR entries to IP ranges from stdin.
+  $ cat /path/to/file1.txt | %[1]s cidr2range
+
+  # Convert CIDR entries to IP ranges from stdin and a file.
+  $ cat /path/to/file1.txt | %[1]s cidr2range /path/to/file2.txt
 
 Options:
   --help, -h
