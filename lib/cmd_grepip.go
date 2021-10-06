4bef0c9c862ac19f4254cdc6f067c5db376c625d
Uman Shahzad <uman@ipinfo.io>
2021-10-06 12:15:06 +0500

    fix grepip output printing logic.
    there were some bugs around this for many reasons,
    but see the diff.

--- lib/cmd_grepip.go
+++ lib/cmd_grepip.go
@@ -99,7 +99,11 @@ func CmdGrepIP(
 
 	// if user hasn't forced no-filename, and we have more than 1 source, then
 	// output file
-	if !f.NoFilename && !(len(args) == 0 || (len(args) == 1 && !isStdin)) {
+	srcCnt := len(args)
+	if isStdin {
+		srcCnt += 1
+	}
+	if !f.NoFilename && srcCnt > 1 {
 		f.NoFilename = false
 	} else {
 		f.NoFilename = true
@@ -271,6 +275,7 @@ func CmdGrepIP(
 				}
 			}
 
+			// no match?
 			if len(matches) == 0 {
 				continue
 			}
@@ -278,33 +283,35 @@ func CmdGrepIP(
 			// print line up to last match.
 			prevMatchEnd := 0
 			for _, m := range matches {
-				// print source.
+				// print source if requested, but only if we're printing
+				// 1 match per line, or this is the first match of the line.
 				if !f.NoFilename && (prevMatchEnd == 0 || f.OnlyMatching) {
 					fmtSrc.Printf("%s:", src)
 				}
 
-				// print pre-match.
+				// print everything up to the current match.
 				if !f.OnlyMatching {
 					fmt.Printf("%s", d[prevMatchEnd:m[0]])
 				}
 
-				// print match.
+				// print the match itself.
 				fmtMatch.Printf("%s", d[m[0]:m[1]])
-				if f.OnlyMatching && prevMatchEnd == 0 && len(matches) > 1 {
+
+				if f.OnlyMatching {
 					fmt.Printf("\n")
 				}
 
 				prevMatchEnd = m[1]
 			}
 
-			// print remaining portion and a newline.
+			// print remaining portion and a newline, if any, and only if we
+			// need to print it.
 			if !f.OnlyMatching {
 				m := matches[len(matches)-1]
 				if m[1] < len(d) {
-					fmt.Printf("%s", d[m[1]:len(d)-1])
+					fmt.Printf("%s", d[m[1]:])
 				}
 			}
-			fmt.Printf("\n")
 		}
 	}
 
