fed827d5971db2b98b077d0bc0296d805179fe99
Umar Farooq <mail.umar.now@gmail.com>
2021-11-25 16:31:32 +0500

    added script for PPA release

---
+++ scripts/release-ppa.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+ROOT=$DIR/..
+
+VSN=$1
+KEY=$2
+
+if [ -z "$VSN" ]; then
+    echo "require version as first parameter" 2>&1
+    exit 1
+fi
+
+if [ -z "$KEY" ]; then
+    echo "require gpg key as second parameter" 2>&1
+    exit 1
+fi
+
+# building the package package 
+cd $ROOT
+debuild -us -uc -S -d 
+
+# signing the package
+cd $ROOT/..
+debsign -k $KEY ipinfo_${VSN}.dsc ipinfo_${VSN}_source.changes
+
+# uploading the package to ppa 
+dput ppa:ipinfo/ppa ipinfo_${VSN}_source.changes
