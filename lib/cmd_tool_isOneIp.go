9944cc2989280f184bc6b7cef16016e03f8b5cf2
harisabdullah <hariskhan1435@gmail.com>
2023-09-07 14:13:34 +0500

    using `net/netip`

--- lib/cmd_tool_isOneIp.go
+++ lib/cmd_tool_isOneIp.go
@@ -3,7 +3,7 @@ package lib
 import (
 	"fmt"
 	"github.com/spf13/pflag"
-	"net"
+	"net/netip"
 )
 
 // CmdToolIsOneIpFlags are flags expected by CmdToolIP2n
@@ -30,7 +30,11 @@ func CmdToolIsOneIp(f CmdToolIsOneIpFlags, args []string, printHelp func()) erro
 		isOneIp := false
 		switch inputType {
 		case INPUT_TYPE_CIDR:
-			isOneIp = CIDRContainsExactlyOneIP(input)
+			prefix, err := netip.ParsePrefix(input)
+			if err != nil {
+				return ErrInvalidInput
+			}
+			isOneIp = prefix.IsSingleIP()
 		case INPUT_TYPE_IP:
 			isOneIp = true
 		case INPUT_TYPE_IP_RANGE:
@@ -43,24 +47,6 @@ func CmdToolIsOneIp(f CmdToolIsOneIpFlags, args []string, printHelp func()) erro
 	}
 
 	return GetInputFrom(args, true, true, op)
-}
-
-// CIDRContainsExactlyOneIP checks whether a CIDR contains exactly one IP
-func CIDRContainsExactlyOneIP(cidrStr string) bool {
-	_, ipnet, err := net.ParseCIDR(cidrStr)
-	if err != nil {
-		return false
-	}
-
-	if ipnet.IP.To4() != nil {
-		ipRange, _ := IPRangeStrFromCIDR(cidrStr)
-		return ipRange.Start == ipRange.End
-	} else if ipnet.IP.To16() != nil {
-		ipRange, _ := IP6RangeStrFromCIDR(cidrStr)
-		return ipRange.Start == ipRange.End
-	}
-
-	return false
 }
 
 func ipRangeContainsExactlyOneIP(ipRangeStr string) bool {
