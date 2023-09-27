bb486909a33cefaa7636e261b6b071f90a3b2c58
abu usama <usamaliaqat5846@gmail.com>
2023-09-27 13:27:56 +0500

    support both `#` and `##` for version headings

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -11,19 +11,18 @@ ROOT=$DIR/..
 CLI=$1
 VERSION=$2
 
-MARKER_PREFIX="##"
 found=0
 
 cat $ROOT/${CLI}/CHANGELOG.md | while read "line"; do
 
     # Find the version heading
-    if [ $found -eq 0 ] && echo "$line" | grep -q "^$MARKER_PREFIX $VERSION$"; then
+    if [ $found -eq 0 ] && (echo "$line" | grep -q -E "^(#|##) $VERSION$"); then
         found=1
         continue
     fi
 
-    # Reaching next delimter - stop
-    if [ $found -eq 1 ] && echo "$line" | grep -q -E "^$MARKER_PREFIX [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"; then
+    # Reaching next delimiter - stop
+    if [ $found -eq 1 ] && (echo "$line" | grep -q -E "^(#|##) [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"); then
         found=0
         break
     fi
