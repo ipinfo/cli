61c540a25dc3e1429597db1a190a83e9a0865947
abu usama <usamaliaqat5846@gmail.com>
2024-01-11 14:53:06 +0500

    updated cli install convenience scripts

--- randip/deb.sh
+++ randip/deb.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
 VSN=1.1.0
+DEFAULT_ARCH=amd64
 
-curl -LO https://github.com/ipinfo/cli/releases/download/randip-${VSN}/randip_${VSN}.deb
-sudo dpkg -i randip_${VSN}.deb
-rm randip_${VSN}.deb
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        ARCH_NAME="amd64"
+        ;;
+    i386|i686)
+        ARCH_NAME="386"
+        ;;
+    aarch64)
+        ARCH_NAME="arm64"
+        ;;
+    armv7l)
+        ARCH_NAME="arm"
+        ;;
+    *)
+        ARCH_NAME=$DEFAULT_ARCH
+        ;;
+esac
+curl -LO https://github.com/ipinfo/cli/releases/download/randip-${VSN}/randip_${VSN}_linux_${ARCH_NAME}.deb
+sudo dpkg -i randip_${VSN}_linux_${ARCH_NAME}.deb
+rm randip_${VSN}_linux_${ARCH_NAME}.deb
 
 echo
 echo 'You can now run `randip`'.
